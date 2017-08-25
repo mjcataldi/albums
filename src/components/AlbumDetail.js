88f7dbef441361da702a560a51410f61ace09ca7
Matthew Cataldi <mcataloe@gmail.com>
2017-08-25 12:35:08 -0500

    Setting up the album view page

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,69 @@
 import React from 'react';
 import { Text } from 'react-native';
-import Card from './card';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
 
 const AlbumDetail = (props) => {
+	// This process is called destructuring.  DESTRUCTURE
+	const { title, artist, thumbnail_image, image } = album;
+	const { 
+		headerContentStyle,
+		headerTextStyle,
+		thumbnailStyle, 
+		thumbnailContainerStyle,
+		imageStyle
+	} = styles; 
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+    	<CardSection style={ headerContentStyle }>
+				<View style={ thumbnailContainerStyle }>
+					<Image 
+						style={ thumbnailStyle } 
+						source={ { uri: thumbnail_image } } />
+				</View>
+
+    		<View style={ headerTextStyle }>
+      		<Text>{props.album.title}</Text>
+      	</View>
+    	</CardSection>
+
+    	<CardSection>
+    		<Image source={ { uri: image } } style={ imageStyle } />
+    	</CardSection>
+
+    	<CardSection>
+    		<Button />
+    	</CardSection>
     </Card>
-  );
+  	);
+	}
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: "space-around"
+	},
+	headerTextStyle: {
+		fontSize: 18,
+	},	
+	thumbnailStyle: {
+		height: 50, 
+		width: 50,
+	},
+	thumbnailContainerStyle: {
+		justifyContent: "center",
+		alignItems: "center",
+		marginLeft: 10,
+		marginRight: 10
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
+	}
 }
 
 export default AlbumDetail;
